445fa632f68690b8d816c0f83c799221223f0254
k3t4 <k3tchll@yandex.ru>
2022-06-16 09:06:56 +0300

    change thumb cluster

--- keyboards/sofle/keymaps/ketch-dev/keymap.c
+++ keyboards/sofle/keymaps/ketch-dev/keymap.c
@@ -40,13 +40,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
+ * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Del  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | Enter|
+ * |Pinky |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | Enter|
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * | TAB  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCTR | LGUI | LCTR | SPC  | / Lower /       \Upper \  |LShift| Bspc | LAlt | RGUI |
+ *            | LALT | LGUI | LCTR | SPC  | / Lower /       \Upper \  |LShift| Bspc | LAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
   KC_PINKY,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,
   KC_TAB,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LALT, KC_LGUI, KC_LCTRL,   KC_SPC, KC_LOWER,      KC_RAISE,  KC_LSFT, KC_BSPC, KC_RGUI, KC_RCTRL
+                 KC_LALT, KC_LGUI, KC_LCTRL,   KC_SPC, KC_LOWER,      KC_LSFT,  KC_BSPC, KC_LSFT, KC_RGUI, KC_RCTRL
 ),
 /*
  * COLEMAK
