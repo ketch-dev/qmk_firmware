f41301160e64337b997c7d34090a622789b6cdf9
Ilya Marchik <ketch.dev@gmail.com>
2024-01-28 11:30:48 +0300

    add HYPR(KC_H) to right rotator

--- keyboards/sofle/keymaps/ketch-dev/keymap.c
+++ keyboards/sofle/keymaps/ketch-dev/keymap.c
@@ -31,21 +31,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   TD(TD_GRV_DHM),
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
   KC_BSPC,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,
-  KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, HYPR(KC_K),     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  TD(TD_RSFT_GAMES),
+  KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, HYPR(KC_K),     HYPR(KC_H), KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  TD(TD_RSFT_GAMES),
                  KC_LALT, KC_LSFT, KC_LGUI, KC_LCTL, KC_SYM,      KC_EXTEND,  KC_SPC, KC_LSFT, KC_LALT, KC_RGUI
 ),
 [_DHM] = LAYOUT(
 	KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   TD(TD_GRV_DHM),
 	KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_DEL,
 	KC_BSPC,  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_ENT,
-	KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, HYPR(KC_K),     XXXXXXX,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+	KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, HYPR(KC_K),     HYPR(KC_H), KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
 									KC_LALT, KC_LSFT, KC_LGUI, KC_LCTL, KC_SYM,      KC_EXTEND,  KC_SPC, KC_LSFT, KC_LALT, KC_LGUI
 ),
 [_GAMES] = LAYOUT(
 	KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_GRV,
 	KC_TAB,   KC_J,   KC_Q,    KC_W,    KC_E,    KC_R,                     KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_DEL,
 	KC_L  ,   KC_LSFT,KC_A,    KC_S,    KC_D,    KC_F,                     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_ENT,
-	KC_U  ,   KC_LCTL,KC_Z,    KC_X,    KC_C,    KC_V, HYPR(KC_K),     XXXXXXX,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  TD(TD_RSFT_GAMES),
+	KC_U  ,   KC_LCTL,KC_Z,    KC_X,    KC_C,    KC_V, HYPR(KC_K),     HYPR(KC_H),KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  TD(TD_RSFT_GAMES),
 									KC_Y   , KC_M   , KC_N   , KC_SPC, KC_I,      KC_EXTEND,  KC_SPC, KC_LSFT, KC_LALT, KC_RGUI
 ),
 [_SYM] = LAYOUT(
