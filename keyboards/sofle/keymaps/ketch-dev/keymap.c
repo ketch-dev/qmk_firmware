cea995628f6161593ab812a971562979c6fa531c
Ilya Marchik <ketch.dev@gmail.com>
2025-03-18 14:17:56 +0100

    add separate keys for Х and Э letters

--- keyboards/sofle/keymaps/ketch-dev/keymap.c
+++ keyboards/sofle/keymaps/ketch-dev/keymap.c
@@ -29,13 +29,12 @@ enum custom_keycodes {
 
 enum tap_dance {
     TD_ARROWS_SYM,
-    TD_Х_Э_Ъ,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
         _______    ,_______ ,_______ ,_______ ,_______        ,_______        ,                /**/                    _______ ,_______ ,_______ ,_______ ,_______ ,_______      ,
-        KC_TAB     ,KC_Q    ,KC_W    ,KC_E    ,KC_R           ,KC_T           ,                /**/                    KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,TD(TD_Х_Э_Ъ) ,
+        KC_QUOT    ,KC_Q    ,KC_W    ,KC_E    ,KC_R           ,KC_T           ,                /**/                    KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_LBRC ,
         KC_ESC     ,KC_A    ,KC_S    ,KC_D    ,KC_F           ,KC_G           ,                /**/                    KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_ENT       ,
         KC_BSPC    ,KC_Z    ,KC_X    ,KC_C    ,KC_V           ,KC_B           ,HYPR(KC_K)    , /**/ HYPR(KC_H)        ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_DEL       ,
                             _______  ,_______ ,KC_MT_CTRL_MEH ,KC_MT_GUI_HYPR ,KC_MT_LSFT_CS , /**/ TD(TD_ARROWS_SYM) ,KC_SPC  ,_______ ,_______ ,_______
@@ -79,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______            ,_______   ,_______ ,_______ ,_______ ,_______           ,          /**/          _______    ,_______  ,_______ ,_______ ,_______ ,_______ ,
         _______            ,KC_1      ,KC_2    ,KC_3    ,KC_4    ,KC_5              ,          /**/          KC_6       ,KC_7     ,KC_8    ,KC_9    ,KC_0    ,_______ ,
         KC_TOGGLE_OVERRIDE ,_______   ,_______ ,_______ ,_______ ,DF(KC_MAP_GMS)    ,          /**/          KC_QWERTY  ,KC_LEFT  ,KC_DOWN ,KC_UP   ,KC_RGHT ,_______ ,
-        _______            ,KC_CAPS   ,KC_PSCR ,KC_INS  ,_______ ,DF(KC_QWERTY_GMS) ,_______ , /**/ _______ ,DF(KC_DHM) ,KC_HOME  ,KC_PGDN ,KC_PGUP ,KC_END  ,_______ ,
+        _______            ,KC_CAPS   ,KC_PSCR ,KC_INS  ,KC_TAB  ,DF(KC_QWERTY_GMS) ,_______ , /**/ _______ ,DF(KC_DHM) ,KC_HOME  ,KC_PGDN ,KC_PGUP ,KC_END  ,_______ ,
                                                 _______ ,_______ ,_______ ,_______  ,KC_FS   , /**/ _______ ,_______    ,_______  ,_______ ,_______
     ),
     [_FS] = LAYOUT(
@@ -243,21 +242,8 @@ void td_arrows_sym_reset(tap_dance_state_t *state, void *user_data) {
     td_state = TD_NONE;
 }
 
-void td_Х_Э_Ъ_finished(tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) register_code(KC_LBRC);
-    else if (state->count == 2) register_code(KC_QUOT);
-    else register_code(KC_RBRC);
-}
-
-void td_Х_Э_Ъ_reset(tap_dance_state_t *state, void *user_data) {
-    unregister_code(KC_LBRC);
-    unregister_code(KC_QUOT);
-    unregister_code(KC_RBRC);
-}
-
 tap_dance_action_t tap_dance_actions[] = {
     [TD_ARROWS_SYM] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_arrows_sym_finished, td_arrows_sym_reset),
-    [TD_Х_Э_Ъ] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_Х_Э_Ъ_finished, td_Х_Э_Ъ_reset),
 };
 
 #ifdef ENCODER_ENABLE
